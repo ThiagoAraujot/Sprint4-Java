cf0a45d13c8a6f08bf3793aa62ba6eae215c806f
Thiago Araujo Vieira <devthiagoaraujo@gmail.com>
2025-04-28 21:57:49 -0300

    inital commit

---
+++ src/main/java/com/fiap/sprint_java/Sprint4JavaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.sprint_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint4JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint4JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/sprint_java/Sprint4JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.sprint_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint4JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
